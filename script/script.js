159113ccbb720e419bc24375b84e9c4eb4478d84
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2024-08-21 15:45:48 -0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,5 @@
+const topLeft = document.getElementById("top-left");
+const topCenter = document.getElementById("top-center");
+const topRight = document.getElementById("top-right");
+const middleLeft = document.getElementById("middle-left");
+const middleCenter = document.getElementById("middl")
